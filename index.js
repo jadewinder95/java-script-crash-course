c81028c786cf37cbbe6574a574efc5ddd3cee3c4
jadewinder95 <jadewinder95@gmail.com>
2025-09-08 20:09:44 -0700

    help

---
+++ index.js
@@ -0,0 +1,19 @@
+//service_acjew8j
+//hJLqa4rsc6P8iEghZ
+//hJLqa4rsc6P8iEghZ
+
+// function contact(event) {
+//     event.preventDefault();
+//     emailjs
+//     .sendform(
+//         'service_acjew8j',
+//     )
+
+// }
+
+const loading = document.querySelector('.modal__overlay--loading');
+const success = document.querySelector('.modal__overlay--success');
+loading.classList.remove('modal__overlay--visible');
+setTimeout(() => {
+    console.log('it worked')
+}, 500);
