52d2ce799ff1a2403a634d9f1964edcecf2c26c5
jadewinder95 <jadewinder95@gmail.com>
2025-09-09 17:23:31 -0700

    help

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 //service_acjew8j
+//template_9yxcq7m
 //hJLqa4rsc6P8iEghZ
-//hJLqa4rsc6P8iEghZ
 
-// function contact(event) {
-//     event.preventDefault();
-//     emailjs
-//     .sendform(
-//         'service_acjew8j',
-//     )
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    emailjs
+      .sendform(
+        'service_acjew8j',
+        "template_9yxcq7m",
+        event.target,
+        'user_hJLqa4rsc6P8iEghZ'
+  ).then(() => {
+    loading.classList.remove('modal__overlay--visible')
+    success.classList =+ ' modal__overlay--visible'
+  }).catch(() => {
+    loading.classList.remove('modal__overlay--visible')
+    alert(
+        'The email service is temporarily unavailable. Please contact me directly at jadewinder95@gmail.com'
+    )
+  })
 
-// }
+}
 
-const loading = document.querySelector('.modal__overlay--loading');
-const success = document.querySelector('.modal__overlay--success');
-loading.classList.remove('modal__overlay--visible');
-setTimeout(() => {
-    console.log('it worked')
-}, 500);
